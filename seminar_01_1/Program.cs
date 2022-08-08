f8b4b2f0f2ab3e9097a0f83a1e1764f7b3a16041
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-08 20:49:44 +0300

    exersize01_square or not square

--- seminar_01_1/Program.cs
+++ seminar_01_1/Program.cs
@@ -1,4 +1,13 @@
-﻿Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int square = number * number;
-Console.WriteLine(square);
+﻿Console.Write("Введите первое число: ");
+int number1  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2  = Convert.ToInt32(Console.ReadLine());
+int square = number2 * number2;
+if(square == number1)
+{
+    Console.WriteLine("Первое число - квадрат второго!");
+}
+else 
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}    
