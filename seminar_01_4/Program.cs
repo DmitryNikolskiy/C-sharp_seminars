c6345648700e28b6ffaaf433c058dda425cdaaf9
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-08 21:25:39 +0300

    exersize04_larger number from 3

---
+++ seminar_01_4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Давайте узнаем какое число больше!");
+Console.Write("Введите первое число: ");
+int number1  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3  = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if(max < number2)
+{
+    max = number2;
+}
+else if(max < number3)
+{
+    max = number3;
+}
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
