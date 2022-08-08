36a9e1a6d7976526c8f2c29ad798637ed98fff9e
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-08 22:32:42 +0300

    exersize08_even numbers from 1 to N

---
+++ seminar_01_8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int N  = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i < N+1)
+{
+    if(i % 2 == 0)
+    {
+    Console.WriteLine(i);
+    }
+    i++;
+}
