f02e51adb7aabe44d7bc227c57f783f49a4bc43e
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-08 20:58:59 +0300

    exersize02_which number larger

---
+++ seminar_01_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Давайте узнаем какое число больше!");
+Console.Write("Введите первое число: ");
+int number1  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2  = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2)
+{
+    Console.WriteLine("Первое число больше второго!");
+}
+else if(number1 < number2)
+{
+    Console.WriteLine("Второе число больше первого!");
+}
+else
+{
+    Console.WriteLine("Числа равны!");
+}    
