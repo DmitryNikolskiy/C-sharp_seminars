d11885eb836e14966e0c9fa16ddf62231f9a8289
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-08 22:07:06 +0300

    exersize06_even number

--- seminar_01_6/Program.cs
+++ seminar_01_6/Program.cs
@@ -1,13 +1,11 @@
-﻿Console.Write("Введите трехзначное число: ");
+﻿Console.WriteLine("Давайте узнаем четное число или нет.");
+Console.Write("Введите число: ");
 int number  = Convert.ToInt32(Console.ReadLine());
-int third = 0;
-if(99 < number & number < 1000)
+if(number % 2 == 0 )
 {
-    third = number % 10;
-    Console.Write("Последняя цифра: ");
-    Console.WriteLine(third);
+    Console.Write("Да!");
 }
 else
 {
-Console.Write("Число не трехзначное!");
+Console.Write("Нет!");
 }
