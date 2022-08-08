71607d3bc6acfd31a03396a8715fccf21df82df9
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-08 21:47:17 +0300

    exersize06_last number

---
+++ seminar_01_6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number  = Convert.ToInt32(Console.ReadLine());
+int third = 0;
+if(99 < number & number < 1000)
+{
+    third = number % 10;
+    Console.Write("Последняя цифра: ");
+    Console.WriteLine(third);
+}
+else
+{
+Console.Write("Число не трехзначное!");
+}
