efa098b15c1d8115cd3fc2d27343eb8f614b5035
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-09 21:06:22 +0300

    exersize010_second digit from number from 100 to 999

---
+++ seminar_02_10/Program.cs
@@ -0,0 +1,4 @@
+﻿int number = new Random().Next (100,1000);
+Console.WriteLine($"Случайное трехзначное число: {number}");
+int digit = number / 10 % 10;
+Console.WriteLine($"Вторая цифра этого числа: {digit}");
