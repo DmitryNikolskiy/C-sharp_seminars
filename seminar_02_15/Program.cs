337266ff7d8f8a3685e351b7d0f00fd388b8f788
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-09 22:25:00 +0300

    exersize015_type of week day

---
+++ seminar_02_15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число для дня недели от 1 до 7:");
+int day = Convert.ToInt32(Console.ReadLine());
+if(day >= 1 & day <= 5)
+{
+    Console.WriteLine("Нет - это будний день!");
+    }
+else if(day == 6 | day == 7)
+{
+    Console.WriteLine("Да - это выходной день!");
+}
+else
+{
+    Console.WriteLine("Такого дня недели нет!");
+}
