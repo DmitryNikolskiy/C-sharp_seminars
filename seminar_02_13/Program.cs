0277d52a7a8d33df7d586705846dcf0ad6000b17
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-09 22:04:05 +0300

    exersize013_third digite

---
+++ seminar_02_13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+string num = Convert.ToString(number);
+int lenght = num.Length;
+int div = Convert.ToInt32(Math.Pow(10, (lenght-3)));
+if(number > 99)
+{
+    Console.WriteLine($"Третья цифра: {number / div % 10}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
