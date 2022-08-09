72eb0606211165f5f48d8a69ada70ba9b016befa
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-09 21:16:28 +0300

    exersize011_without second digit from number from 100 to 999

---
+++ seminar_02_11/Program.cs
@@ -0,0 +1,4 @@
+﻿int number = new Random().Next (100,1000);
+Console.WriteLine($"Случайное трехзначное число: {number}");
+int digit = number / 100 * 10 + number % 10;
+Console.WriteLine($"Новое число (без второ цифры: {digit}");
