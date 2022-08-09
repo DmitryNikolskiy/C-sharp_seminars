f58739d8027be2d031a4893b1515379bd6c70814
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-09 22:10:23 +0300

    exersize014_multiple 7 and 23

---
+++ seminar_02_14/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 23 == 0 && number % 7 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
