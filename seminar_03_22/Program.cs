f98bbbebc7d8655fe3439e12fa96df0bd1db2fe4
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-16 23:17:41 +0300

    exersize22_N_square

---
+++ seminar_03_22/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число N: ");
+int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int i = 1;
+while (i < N)
+{
+    Console.Write($"{Math.Pow(i, 2)}, ");
+    i++;
+}
+Console.Write(Math.Pow(N, 2));
