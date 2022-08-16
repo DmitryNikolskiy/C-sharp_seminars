11957787f1b55026471c1bfe97243402b0615705
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-16 21:46:03 +0300

    exersize19_palindrom

---
+++ seminar_03_19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int number5 = Convert.ToInt32(Console.ReadLine());
+string number = Convert.ToString(Math.Abs(number5));
+int[] array = new int [5];
+int i=0;
+while (i < 5)
+{
+    array[i] = int.Parse(number[i].ToString());
+    i++;
+}
+if(array[0] == array[4] && array[1] == array[3])
+{
+    Console.WriteLine("Число - палиндром");
+}
+else
+{ 
+   Console.WriteLine("Число - не палиндром"); 
+}
