3fd3d7ff6f2e2dfe0ab72ad3d7255d43757fedd0
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-16 23:46:12 +0300

    exersize23_N_cubic

---
+++ seminar_03_23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[Math.Abs(N)];
+int number = Math.Abs(N)/N;
+int i = 0;
+while (i < Math.Abs(N)-1)
+{
+    array[i] = Convert.ToInt32(Math.Pow(number+i*number, 3));
+    Console.Write($"{array[i]}, ");
+    i++;
+}
+Console.Write(Math.Pow(N, 3));
