5e52f92be4ad25c9343155fbbb3b67819f79c4f2
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-17 20:46:16 +0300

    exersize25_A_power_B

---
+++ seminar_04_25/Program.cs
@@ -0,0 +1,16 @@
+﻿int Degree(int a, int b)
+{
+   int result = 1;
+    for (int i = 0; i<b; i++)
+    {
+        result = result*a;
+    } 
+    return result;
+}
+
+Console.Write("Введите число А: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int deg = Convert.ToInt32(Console.ReadLine());
+int power = Degree(number, deg);
+Console.WriteLine($"A в степени B = {power}");
