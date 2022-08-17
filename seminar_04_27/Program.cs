4d290c71b342adae5f468e857f74774939221277
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-17 21:47:21 +0300

    exersize27_sum_of_digit

---
+++ seminar_04_27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+string numbertext = Convert.ToString(Math.Abs(number));
+int length = numbertext.Length;
+int sum = int.Parse(numbertext[0].ToString());
+for (int i = 1; i < length; i++)
+{
+        sum = sum + int.Parse(numbertext[i].ToString());
+}
+Console.WriteLine($"Сумма чисел {sum}");
