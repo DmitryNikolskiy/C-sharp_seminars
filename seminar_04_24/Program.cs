b98135afc6a4ecf92a8407db4d146bb4c7bcecb9
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-17 20:19:39 +0300

    exersize24_sum_of_numbers_in_N

---
+++ seminar_04_24/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Ввод числа А: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i<=number; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine($"Результат работы программы = {sum}");
