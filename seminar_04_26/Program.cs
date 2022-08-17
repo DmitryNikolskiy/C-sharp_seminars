dc4495b863b475c2c01542cb53e823b38b86c47b
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-17 21:01:02 +0300

    exersize26_number_of_digits

---
+++ seminar_04_26/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.Write("Введите число: ");
+string number = Convert.ToString(Math.Abs(Convert.ToInt32(Console.ReadLine())));
+Console.WriteLine($"Количество цифр в этом числе - {number.Length}");
