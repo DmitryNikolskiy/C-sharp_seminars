882906029a8288017b0628386d3fb25a840a9c8e
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-17 23:07:43 +0300

    exersize30_array_[]_0_or_1

---
+++ seminar_04_30/Program.cs
@@ -0,0 +1,19 @@
+﻿void Fill(int[] massive1)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        massive1[i] = new Random().Next (0, 2);
+    }
+}
+void Print(int[] massive2)
+{
+    Console.Write("[");
+    for (int j = 0; j < 7; j++)
+    {
+        Console.Write($"{massive2[j]}, ");
+    }
+    Console.Write($"{massive2[7]}]");
+}
+int[] array = new int[8];
+Fill(array);
+Print(array);
