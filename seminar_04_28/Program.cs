420a58e4bf52b308ff0bed0e671e7d860c0335cb
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-17 22:16:44 +0300

    exersize28_Multiplication_1_N

---
+++ seminar_04_28/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число от 1 до N");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1)
+{
+    Console.WriteLine("Число меньше 1, попробуйте езе раз");
+}
+else
+{
+    int result = number;
+    for (int i = 1; i < number; i++)
+    {
+        result = result * i;
+    }
+    Console.WriteLine($"Произведение чисел {result}");
+}
