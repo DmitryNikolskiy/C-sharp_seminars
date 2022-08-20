bcbf600cbcbf2562f9d3c38c91bf2d6950ff5c79
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-20 09:15:17 +0300

    exersize38_difference_max_min

---
+++ seminar_05_38/Program.cs
@@ -0,0 +1,46 @@
+﻿void Fill(double[] massive1, int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"Введите значение {i+1} элемента массива: ");
+        massive1[i] = Convert.ToDouble(Console.ReadLine());
+    }
+}
+void Print(double[] massive2, int numpr)
+{
+    Console.Write("[");
+    for (int j = 0; j < numpr-1; j++)
+    {
+        Console.Write($"{massive2[j]}, ");
+    }
+    Console.Write($"{massive2[numpr-1]}]");
+}
+Console.Write("Введите размерность массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1)
+{
+    Console.WriteLine("Необходимо значение больше 0");
+}
+else
+{
+    Console.WriteLine();
+    double[] array = new double[number];
+    Fill(array, number);
+    Console.WriteLine("Вы сформировали массив: ");
+    Print(array, number);
+    Console.WriteLine();
+    double min = array[0];
+    double max = array[0];
+    for (int k = 1; k < number; k++)
+    {
+        if (array[k] < min) 
+        {
+            min = array[k];
+        }
+        else if (array[k] > max) 
+        {
+            max = array[k];
+        }
+    }
+    Console.WriteLine($"Разость между максимальным и минимальными элементами массива: {max - min}");
+}
