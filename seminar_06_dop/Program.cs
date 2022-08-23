907c153b75b3dc51c9f83781a5776a4615beab56
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-24 00:00:15 +0300

    exersize45_dop_factorial

---
+++ seminar_06_dop/Program.cs
@@ -0,0 +1,10 @@
+﻿int Multiplicate(int N)
+{
+    if (N == 1) return 1;
+    else return Multiplicate(N-1) * N;
+}
+
+Console.Write("Введите положительное целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) { Console.WriteLine("Число меньше 1"); }
+else { Console.WriteLine($"Произведение чисел от 1 до {number}: {Multiplicate(number)}"); }
