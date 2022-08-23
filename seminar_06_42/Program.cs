a88853993f4063cffff7c289ef3dc4a43a483ef5
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-23 22:29:10 +0300

    exersize42_print_binary

---
+++ seminar_06_42/Program.cs
@@ -0,0 +1,10 @@
+﻿void PrintBinary(int N)
+{
+    if (N <= 0) return;
+    PrintBinary(N/2);
+    Console.Write(N%2);
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintBinary(number);
