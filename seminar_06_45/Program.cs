2823424a1184ee2b8c5c323235a58a7901bc249a
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-23 23:45:55 +0300

    exersize45_Array_Copy_Elements

---
+++ seminar_06_45/Program.cs
@@ -0,0 +1,25 @@
+﻿const int ArraySize = 5;
+const int leftRange = -10;
+const int rightRange = 10;
+
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int [] arr = new int [size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange+1);
+    }
+    return arr;
+}
+
+int[] array = FillArray(ArraySize, leftRange, rightRange);
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+int[] CopyArray = new int[array.Length];
+for (int j = 0; j < array.Length; j++)
+{
+    CopyArray[j] = array[j];
+        
+}
+
+Console.WriteLine('[' + string.Join(", ", CopyArray) + ']');
