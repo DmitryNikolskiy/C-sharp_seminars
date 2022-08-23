24c6917c382c49fc9e4d1c508e19f05597be32d4
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-23 22:52:41 +0300

    exersize43_coordinate_of_crossing

---
+++ seminar_06_43/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите коэффициент B1: ");
+double B1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффициент K1: ");
+double K1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффициент B2: ");
+double B2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффициент K2: ");
+double K2 = Convert.ToDouble(Console.ReadLine());
+
+if (K1 != K2)
+{
+    double x = (B1-B2) / (K2 - K1);
+    double y = (K1 * x + B1);
+    Console.WriteLine($"Точка пересечения двух прямых: ({x}, {y})");
+}
+else {Console.WriteLine("Прямые не пересекаются");}
