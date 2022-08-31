44dd91c5c1f73766403fb77a1483c30a9d9b30ce
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-31 21:16:52 +0300

    exersize46_2m_array

---
+++ seminar_07_46/Program.cs
@@ -0,0 +1,24 @@
+﻿void Fill(int[,] massive1)
+{
+    for (int i = 0; i < 3; i++)
+    {
+            for (int j = 0; j < 4; j++)
+            {
+                massive1[i,j] = new Random().Next (1, 1000);
+            }
+    }
+}
+void Print(int[,] massive2)
+{
+    for (int k = 0; k < 3; k++)
+    {
+        for (int l = 0; l < 4; l++)
+        {
+            Console.Write($"{massive2[k,l]} ");
+        }
+        Console.WriteLine(" ");
+    }
+}
+int[,] array = new int[3,4];
+Fill(array);
+Print(array);
