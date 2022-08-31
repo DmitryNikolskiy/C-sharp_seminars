14359aa5ceaca82edfd0d103af6ced92f604eb7a
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-08-31 22:06:38 +0300

    exersize47_matrix_random_double

---
+++ seminar_07_47/Program.cs
@@ -0,0 +1,36 @@
+﻿void Fill(Double[,] massive1, int m1, int n1)
+{
+    for (int i = 0; i < m1; i++)
+    {
+            for (int j = 0; j < n1; j++)
+            {
+                massive1[i,j] = new Random().Next(-1000, 1000) / 100.0; // или new Random().NextDouble() * 100;
+            }
+    }
+}
+void Print(Double[,] massive2, int m2, int n2)
+{
+    for (int k = 0; k < m2; k++)
+    {
+        for (int l = 0; l < n2; l++)
+        {
+            Console.Write($"{massive2[k,l]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите число строк для массива");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов для массива");
+int n = Convert.ToInt32(Console.ReadLine());
+if (m < 1 || n < 1)
+{
+    Console.WriteLine("Число строк и столбцов болжно быть больше 1, попробуйте езе раз");
+}
+else
+{
+Double[,] array = new Double[m,n];
+Fill(array, m, n);
+Print(array, m, n);
+}
