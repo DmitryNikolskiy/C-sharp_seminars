f840c3b1e956e5ad09600b5fd200c265732f1539
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-09-04 13:42:36 +0300

    exersize68_Akkerman

---
+++ seminar_09_68/Program.cs
@@ -0,0 +1,12 @@
+﻿int FAkkerman(int N, int M)
+{
+    if (N == 0) return M + 1;
+    else if (N > 0 && M == 0) return FAkkerman(N-1,1); 
+    else return FAkkerman(N-1, FAkkerman(N, M-1));
+}   
+Console.Write("Введите положительное целое число - первый аргумент функции Аккермана: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите положительное целое число - второй аргумент функции Аккермана: ");
+int second = Convert.ToInt32(Console.ReadLine());
+if (first < 0 || second < 0) { Console.WriteLine("Необходимо ввести корректные аргументы"); }
+else { Console.WriteLine($"Значение функции Аккермана с аргументами ({first}, {second}): {FAkkerman(first, second)}"); }
