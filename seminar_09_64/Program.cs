cbb01eba4fe3b78042402ceb1f4729be010d1008
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-09-04 12:30:21 +0300

    exersize64_SequenceReverse

---
+++ seminar_09_64/Program.cs
@@ -0,0 +1,14 @@
+﻿void Sequence(int N)
+{
+    if (N < 1) 
+    {
+        return;
+    }
+    Console.Write($"{N} ");
+    Sequence(N-1);
+
+}
+
+Console.Write("Введите положительное целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Sequence(number);
