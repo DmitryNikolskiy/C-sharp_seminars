86e5ee9077c64f9697da2cce6087ba8de7949857
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-09-04 12:00:55 +0300

    exersize69_power

---
+++ seminar_09_69/Program.cs
@@ -0,0 +1,14 @@
+﻿int Pow(int a, int b)
+{
+    if (b == 0) 
+    {
+        return 1;
+    }
+    return a*Pow(a,b-1);
+}
+
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Pow(A, B));
