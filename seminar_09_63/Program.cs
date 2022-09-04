4bba4ce67fc06a20803bd885d59fa22bf036d278
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-09-04 11:26:24 +0300

    exersize63_sequence

---
+++ seminar_09_63/Program.cs
@@ -0,0 +1,13 @@
+﻿void Sequence(int N)
+{
+    if (N < 1) 
+    {
+        return;
+    }
+    Sequence(N-1);
+    Console.Write($"{N} ");
+}
+
+Console.Write("Введите положительное целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Sequence(number);
