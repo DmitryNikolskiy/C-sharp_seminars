d75e88be080820e180600297f6d139e8b2367db4
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-09-04 13:27:17 +0300

    exersize67_sum_of_digits

---
+++ seminar_09_67/Program.cs
@@ -0,0 +1,15 @@
+﻿int SumDigit(int N)
+{
+    if (N / 10 > 0 ) 
+    {
+        return SumDigit(N / 10) + N % 10;
+    }
+    else return N;
+}
+
+Console.Write("Введите положительное целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1) 
+Console.WriteLine("Необходимо ввести корректное число");
+else 
+Console.WriteLine($"Сумма цифр в числе {number}: {SumDigit(number)}");
